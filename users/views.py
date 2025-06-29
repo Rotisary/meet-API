931540e5b7e14bcb61185510d49f5761c0c11b5a
Oladotun Kolapo <oladotunkolapo@gmail.com>
2025-06-29 17:46:04 +0100

    configured the home page to rediect to the Postman documentation URL

--- users/views.py
+++ users/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 
 def docs_view(request):
-    return render(request, 'users/docs.html')
+    return HttpResponseRedirect(
+        "https://documenter.getpostman.com/view/33652201/2sB2xFgTgT#meet-api-documentation"
+    )  # Redirect to the Postman documentation URL
